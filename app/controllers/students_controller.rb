bfd747a3c3e34097437d71b3a352e910d51bc49a
JawaraGordon <jawaragordon@icloud.com>
2022-07-12 13:42:57 -0400

    completed assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,13 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    students = params[:name] ? Student.by_name(params[:name]) : Student.all  
+render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+  render json: student
   end
 
 end
